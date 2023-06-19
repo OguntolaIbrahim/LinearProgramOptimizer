bb95561f1cecac3f1a9c60d5a0843b621c290ebd
OguntolaIbrahim <46747732+OguntolaIbrahim@users.noreply.github.com>
2023-06-19 05:43:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ LONG_DESCRIPTION = 'Optimization models for solving Linear Programming Problems'
 
 # Setting up
 setup(
-    name="vidstream",
+    name"LinearProgramOptimizer",
     version=VERSION,
     author="OguntolaIbrahim",
     author_email="<oibrahimopeyemi@yahoo.com>",
